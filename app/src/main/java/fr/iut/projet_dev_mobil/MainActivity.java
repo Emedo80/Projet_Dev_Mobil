2c710d41910bb5dd6f99f8b6faea387bdca1635f
Emedo <emeric.dorion@gmail.com>
2021-03-04 18:25:30 +0100

    [*] Finished Layouts

--- app/src/main/java/fr/iut/projet_dev_mobil/MainActivity.java
+++ app/src/main/java/fr/iut/projet_dev_mobil/MainActivity.java
@@ -3,6 +3,7 @@ package fr.iut.projet_dev_mobil;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void ClickConnexion(View view){
+        setContentView(R.layout.connexion);
+    }
+
+    public void ClickInscription(View view){
+        setContentView(R.layout.inscription);
     }
 }
