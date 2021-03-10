a0ef8b42acd1a3547f11b8cc4ccbb68bc461046a
FrancoisLGF <francois.le.goff@etud.u-picardie.fr>
2021-03-10 15:09:04 +0100

    [+] Début de la création du jeu

--- app/src/main/java/fr/iut/projet_dev_mobil/color_memory.java
+++ app/src/main/java/fr/iut/projet_dev_mobil/color_memory.java
@@ -3,12 +3,57 @@ package fr.iut.projet_dev_mobil;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class color_memory extends AppCompatActivity {
+    Button Blue;
+    Button Red;
+    Button Yellow;
+    Button Green;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color_memory);
+
+        Blue = findViewById(R.id.Blue);
+        Red = findViewById(R.id.Red);
+        Yellow = findViewById(R.id.Yellow);
+        Green = findViewById(R.id.Green);
+
+        Blue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getButton(1);
+            }
+        });
+
+        Red.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getButton(2);
+            }
+        });
+
+        Yellow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getButton(3);
+            }
+        });
+
+        Green.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getButton(4);
+            }
+        }
+
+
+    }
+
+    private void getButton(int button_pushed) {
+
     }
 }
