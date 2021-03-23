01ee8e21fcf9e1e2c50a715cfa9925076004ba05
FrancoisLGF <francois.le.goff@etud.u-picardie.fr>
2021-03-23 22:21:57 +0100

    [*]Jeu terminé mais fonctionne pas

--- app/src/main/java/fr/iut/projet_dev_mobil/color_memory.java
+++ app/src/main/java/fr/iut/projet_dev_mobil/color_memory.java
@@ -2,7 +2,10 @@ package fr.iut.projet_dev_mobil;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 
@@ -11,8 +14,8 @@ public class color_memory extends AppCompatActivity {
     Button Red;
     Button Yellow;
     Button Green;
-    public int position = 0;
-    public int tableau[] = new int[10];
+    public int sequence = 0;
+    public int table[] = new int[10];
     public int life = 2;
 
     @Override
@@ -26,16 +29,9 @@ public class color_memory extends AppCompatActivity {
         Green = findViewById(R.id.Green);
 
         for(int i=0;i<10;i++){
-            tableau[i] = ((int) Math.random() * 4  + 1); // choisit la première couleur
+            table[i] = ((int) Math.random() * 4  + 1); // choisit la première couleur
         }
 
-
-        Blue.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getButton(1);
-            }
-        });
 
         Red.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,12 +57,25 @@ public class color_memory extends AppCompatActivity {
 
     }
 
+    public void sequence_show(int sequence_end){
+        for(int i=0; i<=sequence_end;i++){
+
+            try{
+                Thread.sleep(1000);
+            }
+            catch(InterruptedException e){
+                e.printStackTrace();
+            }
+            button_show(table[i]);
+        }
+    }
 
 
-    public void getButton(int button_pushed) { // incrémentation de la séquence si le bouton est le bon (revoir nom variable)
-        for (int i=0;i<position;i++){
-            if(tableau[position] == button_pushed){
-                position++;
+
+    public void getButton(int button_pushed) { // incrémentation de la séquence si le bouton est le bon
+        for (int i=0;i<sequence;i++){
+            if(table[sequence] == button_pushed){
+                sequence++;
             }
             else{
                 life -= 1;
@@ -78,15 +87,48 @@ public class color_memory extends AppCompatActivity {
 
     public void button_show (int button_pushed){ //Si premier bouton = bleu alors changer la couleur du bleu pour le mettre en évidence
          if(button_pushed == 1){
-            Blue.setBackgroundResource(R.drawable.blue1);
+            Blue.setBackgroundColor(Color.BLUE);
             Handler handler = new Handler();
             handler.postDelayed(new Runnable(){
                 public void run() {
-                    Blue.setBackgroundResource((R.drawable.blue)); // Ne fonctionne pas, à reprendre
+                    Blue.setBackgroundColor(Color.BLUE);
                 }
-            })
+            }, 200);
         }
+
+         else if (button_pushed == 2){
+             Red.setBackgroundColor(Color.RED);
+             Handler handler = new Handler();
+             handler.postDelayed(new Runnable(){
+                 public void run() {
+                     Red.setBackgroundColor(Color.RED);
+                 }
+             }, 200);
+         }
+
+         else if (button_pushed == 3){
+             Yellow.setBackgroundColor(Color.YELLOW);
+             Handler handler = new Handler();
+             handler.postDelayed(new Runnable(){
+                 public void run() {
+                     Yellow.setBackgroundColor(Color.YELLOW);
+                 }
+             }, 200);
+         }
+
+         else if (button_pushed ==4){
+             Green.setBackgroundColor(Color.GREEN);
+             Handler handler = new Handler();
+             handler.postDelayed(new Runnable(){
+                 public void run() {
+                     Green.setBackgroundColor(Color.GREEN);
+                 }
+             }, 200);
+         }
+
+
     }
 
 
 }
+
